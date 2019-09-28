974c01e412a45c5fcab9cfcbf4f641e98025a830
su-u <aaaiiiuuu35@gmail.com>
2019-09-28 08:49:38 +0000

    Up

--- app/main.go
+++ app/main.go
@@ -3,11 +3,22 @@ package main
 import (
     "fmt"
     "net/http"
+    "log"
+    "html/template"
 
+    
     "github.com/gorilla/mux"
 )
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
+    t := template.Must(template.ParseFiles("templates/temp001.html.tpl"))
+    str := "unko"
+
+    // テンプレートを描画
+    if err := t.ExecuteTemplate(w, "temp001.html.tpl", str); err != nil {
+        log.Fatal(err)
+    }
+
     fmt.Println("Hello world!")
 }
 
