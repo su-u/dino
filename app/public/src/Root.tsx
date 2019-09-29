6ca289be285065bd9b7fc8f6cb98e0201c7a9327
su-u <aaaiiiuuu35@gmail.com>
2019-09-29 08:15:19 +0000

    Feat: テストreact アプリ追加

---
+++ app/public/src/Root.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { Provider } from "react-redux";
+import App from "./containers/App";
+
+export default ({ store }) => (
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
