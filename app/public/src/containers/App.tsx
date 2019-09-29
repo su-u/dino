4448dec6739f222c0d22bd1bed1a34d6df6371f0
su-u <aaaiiiuuu35@gmail.com>
2019-09-29 10:19:30 +0000

    Update: コード整理

--- app/public/src/containers/App.tsx
+++ app/public/src/containers/App.tsx
@@ -1,48 +1,11 @@
 import * as React from "react";
-import { bindActionCreators } from "redux";
-import { connect, Dispatch } from "react-redux";
-import styled from "styled-components";
-import * as App_actions from "../actions/app";
 
-interface Props {
-  name: string;
-  app_actions: any;
+
+// Load our simple functions that manage scene/game state
+
+interface State{
 }
 
-export class App extends React.Component<Props> {
-  render() {
-    const { app_actions, name } = this.props;
+export default class App extends React.Component<State> {
 
-    return (
-      <Container>
-        {name ? (
-          `${name} さん、こんにちは。`
-        ) : (
-          <button onClick={() => app_actions.login("test")}>こんにちは</button>
-        )}
-      </Container>
-    );
-  }
 }
-
-function mapStateToProps(state: any) {
-  return {
-    name: state.app.get("login_user_name")
-  };
-}
-
-function mapDispatchToProps(dispatch: Dispatch) {
-  return {
-    app_actions: bindActionCreators(App_actions, dispatch)
-  };
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
-
-const Container = styled.div`
-  max-width: 800px;
-  margin: 0 auto;
-`;
